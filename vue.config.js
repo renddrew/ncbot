d2c33627f82dc62b0b70c73b2e7eb8ee3dafffd5
Andrew Rendall <rend.drew@gmail.com>
2021-01-19 11:21:12 -0500

    fix dist dir

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 module.exports = {
     lintOnSave: false,
-    outputDir: path.resolve(__dirname, "./../dist"),
+    //outputDir: path.resolve(__dirname, "./../dist"),
     devServer: {
         port: 8081
     }
