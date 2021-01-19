0d3385e27740580517d935dc80dfa390e55b16df
Andrew Rendall <rend.drew@gmail.com>
2021-01-19 17:35:53 -0500

    items for averaging multi trades

--- http-server.js
+++ http-server.js
@@ -17,7 +17,7 @@ const binance = new Binance().options({
 
 app.use(express.static('dist'))
 
-app.post('/getTrades', function(req, res) {
+app.post('/getTrades', (req, res) => {
   binance.trades("BTCUSDT", (error, trades, symbol) => {
 
     trades = trades.sort((a, b) => {
@@ -33,23 +33,47 @@ app.post('/getTrades', function(req, res) {
       "time":1610980772825,
     */
 
+    let filteredTrades = [];
     let lastTradeId = null;
-    
-    // for (let i = 0; i < trades.length; i++) {
-    //   if (lastTradeId == trades[i].orderId) {
-    //     groupedTrade.price = (groupedTrade.price + trades[i].price) / 2)
-    //   } else {
-    //     let groupedTrade = trades[i];
-    //   }
-    //   lastTradeId = trades[i].orderId;
-      
-    // }
+    let groupedTrades = [];
 
+    function formatNum(num, decimals) {
+      return parseFloat(parseFloat(num).toFixed(decimals));
+    }    
+  
+    for (let i = 0; i < trades.length; i++) {
+      // always add item to grouped trades, whether has match or not yet
+      groupedTrades.push(trades[i])
+      let lastTrade = trades[i-1];
 
-    res.send({trades})
-    //console.info(symbol+" trade history", trades)
-    return;;
+      if (lastTradeId != trades[i].orderId && i !== 0) {
+        // do averaging for each grouped trade item
+        let averagedFromGroup = {price: 0, qty: 0, quoteQty: 0, commission: 0}
+        for (let gi = 0; gi < groupedTrades.length; gi++) {
+          // add to averagedFromGroup properties
+          averagedFromGroup.price += formatNum(lastTrade.price, 2);
+          averagedFromGroup.qty += formatNum(lastTrade.qty, 6);
+          averagedFromGroup.quoteQty += formatNum(lastTrade.quoteQty, 2);
+          averagedFromGroup.commission += formatNum(lastTrade.commission, 6);
+        }
+        // do averaged from group division based on length of averagedFromGroup
+        let tradeGroupAveraged = lastTrade;
+        for (var prop in averagedFromGroup) {
+          if (!averagedFromGroup.hasOwnProperty(prop)) continue;
+          tradeGroupAveraged[prop] = averagedFromGroup[prop] / groupedTrades.length;
+        }
+
+        // push single grouped trade to filteredTrades
+        filteredTrades.push(tradeGroupAveraged);
+        groupedTrades = [];
+      }
+
+      lastTradeId = trades[i].orderId;
+    }
+
+    res.send({trades: filteredTrades, tradesAll: trades})
   });
 });
+
 
 module.exports = app;
