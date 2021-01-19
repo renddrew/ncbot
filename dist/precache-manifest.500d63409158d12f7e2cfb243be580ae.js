4c4cfd84b46cb950ff5f463e568893fc8a9b9d2c
Andrew Rendall <rend.drew@gmail.com>
2021-01-19 10:06:53 -0500

    entire project init

---
+++ dist/precache-manifest.500d63409158d12f7e2cfb243be580ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703fb76069376e5bcbe7",
+    "url": "/css/app.ca936927.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9114bab16be60f641da5415f89c2c7b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ede9c61a26f519eb2e56",
+    "url": "/js/about.65cf4fb4.js"
+  },
+  {
+    "revision": "703fb76069376e5bcbe7",
+    "url": "/js/app.1d7b78af.js"
+  },
+  {
+    "revision": "a69b552fd11d31335110",
+    "url": "/js/chunk-vendors.dbcb99fc.js"
+  },
+  {
+    "revision": "c60b6b6054d13314c5ca5976b9c1151e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
