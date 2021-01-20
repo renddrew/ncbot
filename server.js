23381a13a2f5540bfe42b8c09f3cbe84cf2c5ea0
Andrew Rendall <rend.drew@gmail.com>
2021-01-20 14:26:40 -0500

    cleaups

--- server.js
+++ server.js
@@ -37,16 +37,14 @@ binance.websockets.chart("BTCUSDT", "1m", (symbol, interval, chart) => {
   data = utils.formatNum(last, 2);
 });
 
-wss.on('connection', function connection (ws) {
-  // ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-     // if (client.readyState === WebSocket.OPEN) {
-        setInterval(() => {
-          client.send(JSON.stringify(data));
-        }, 50);
-     // }
-    });
-  // });git 
+wss.on('connection', ws => {
+  wss.clients.forEach(client => {
+    setInterval(() => {
+      client.send(JSON.stringify(data));
+    }, 50);
+  });
+  // ws.on('message', incoming(data => {
+  // });
 });
 
 server.listen(process.env.PORT, function() {
